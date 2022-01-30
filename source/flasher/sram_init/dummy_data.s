70234b23531d1d6b8dc8bb5281cb5debe0d2a2b0
Willems Davy <joyrider3774@hotmail.com>
2022-01-30 06:37:20 +0100

    Batteryless saving requires 4 rom banks initialized + optimize Makefile

--- source/flasher/sram_init/dummy_data.s
+++ source/flasher/sram_init/dummy_data.s
@@ -1,6 +1,25 @@
+; Character: 4 banks of 8k (0x2000) filled with 0xff because flasher always uses multiple of 4 rom banks because of 64k sector size of the chip
+
 .area _CODE_4
 
-.rept 0x2000 
+.rept 0x4000
 	.db 0xff
 .endm
 
+.area _CODE_5
+
+.rept 0x4000 
+	.db 0xff
+.endm
+
+.area _CODE_6
+
+.rept 0x4000 
+	.db 0xff
+.endm
+
+.area _CODE_7
+
+.rept 0x4000 
+	.db 0xff
+.endm
